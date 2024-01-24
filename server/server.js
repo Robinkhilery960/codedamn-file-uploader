bf5f9c122e2cdc375a844dd04ee5dae545dde92a
Robin Khilery <96528134+Robinkhilery960@users.noreply.github.com>
2024-01-24 07:36:23 +0000

    multer storage code  changed

--- server/server.js
+++ server/server.js
@@ -7,13 +7,15 @@ const app = express();
 app.use(express.static('../client'));
 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-      cb(null, './uploads/');
-    },
-    filename: function(req, file, cb) {
-      cb(null, new Date().toISOString() + file.originalname);
-    }
-  });
+  destination: function (req,res,cb){
+      cb(null, "uploads/");   
+  },
+  filename: function (req,file,cb) {
+      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      const filename = file.originalname.split(".")[0];
+      cb(null,filename + "-" + uniqueSuffix + ".png");
+  },
+});
   
 const upload = multer({ storage: storage })
   
